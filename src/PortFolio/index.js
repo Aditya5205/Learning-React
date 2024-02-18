143fbe786b6e17094e9675c45900c8796a6e1af5
Aditya <adityasingh19122002@gmail.com>
2024-02-18 22:40:06 +0530

    Remove Portfolio and make a new repo for that

--- src/PortFolio/index.js
+++
@@ -1,7 +0,0 @@
-import Home from "./Home";
-import Header_new from "./Header_new";
-import About from "./About";
-import Projects from "./Projects";
-import Contact from "./Contact";
-
-export { Home, Header_new, About, Projects, Contact };
--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,7 @@ import "./index.css";
 import Tic_tac_toe from "./Tic_tac_toe";
 import Dice_game from "./Dice_game";
 import ToDoLocal from "./ToDoLocal";
-import PortfolioLayout from "./PortFolio/PortfolioLayout";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import {
-  Home,
-  Header_new,
-  About,
-  Projects,
-  Contact,
-} from "./PortFolio/index.js";
 
 // const router = createBrowserRouter([
 //   {
@@ -45,11 +37,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <Tic_tac_toe /> */}
     {/* <Dice_game /> */}
-    {/* <Game /> */}
+    <Game />
     {/* <Currency_Generator /> */}
     {/* <ToDoLocal /> */}
     {/* <RouterProvider router={router} /> */}
-    <PortfolioLayout />
+    {/* <PortfolioLayout /> */}
     {/* <Projects /> */}
   </React.StrictMode>,
 );
